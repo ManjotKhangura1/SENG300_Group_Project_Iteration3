8f105d065b1a8e16693a11f7c9c72701e7ab2050
Thanh Hien Nguyen-Mai <47198098+FabulousFritz@users.noreply.github.com>
2021-04-04 15:43:47 -0600

    does the refill dispensers class

---
+++ SelfCheckoutSystem - Software/src/RefillDispensers.java
@@ -0,0 +1,88 @@
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+import org.lsmr.selfcheckout.Banknote;
+import org.lsmr.selfcheckout.Coin;
+import org.lsmr.selfcheckout.devices.BanknoteDispenser;
+import org.lsmr.selfcheckout.devices.CoinDispenser;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SimulationException;
+
+public class RefillDispensers {
+	public SelfCheckoutStation station;
+	public Currency currency;
+
+	/**
+	 * Refill Dispensers constructor
+	 * 
+	 * @param station  The SelfCheckoutStation that is currently being used
+	 * @param currency The kind of currency permitted
+	 */
+	public RefillDispensers(SelfCheckoutStation station, Currency currency) {
+		// throws and exception if station given is null
+		if (station == null) {
+			throw new SimulationException(new NullPointerException("station is null"));
+		}
+		this.station = station;
+
+		// throws and exception if station given is null
+		if (currency == null) {
+			throw new SimulationException(new NullPointerException("currency is null"));
+		}
+		this.currency = currency;
+	}
+
+	/**
+	 * Reloads a specified coin dispenser
+	 * @param coinDenomination the denomination of the dispenser to be reloaded
+	 */
+	public void refillCoin(BigDecimal coinDenomination) {
+		int amountToRefill;
+		CoinDispenser coinDispenser;
+
+		//finds the coin dispenser correlated to the given coin denomination
+		coinDispenser = station.coinDispensers.get(coinDenomination);
+
+		try {
+			//finds how many more coins are needed before the coin dispenser reaches capacity
+			amountToRefill = coinDispenser.getCapacity() - coinDispenser.size();
+
+			//refills the dispenser until it reaches capacity
+			for (int i = 0; i < amountToRefill; i++) {
+				Coin coinToLoad = new Coin(coinDenomination, currency);
+				coinDispenser.load(coinToLoad);
+			}
+
+		} catch (SimulationException | OverloadException e) {
+			throw new SimulationException(e);
+		}
+	}
+	
+	/**
+	 * Reloads a specified banknote dispenser
+	 * @param banknoteDenomination the denomination of the dispenser to be reloaded
+	 */
+	public void refillBanknote(int banknoteDenomination) {
+		int amountToRefill;
+		BanknoteDispenser banknoteDispenser;
+
+		//finds the banknote dispenser correlated to the given banknote denomination
+		banknoteDispenser = station.banknoteDispensers.get(banknoteDenomination);
+
+		try {
+			//finds how many more banknotes are needed before the banknote dispenser reaches capacity
+			amountToRefill = banknoteDispenser.getCapacity() - banknoteDispenser.size();
+
+			//refills the dispenser until it reaches capacity
+			for (int i = 0; i < amountToRefill; i++) {
+				Banknote banknoteToLoad = new Banknote(banknoteDenomination,currency);
+				 banknoteDispenser.load(banknoteToLoad);
+			}
+
+		} catch (SimulationException | OverloadException e) {
+			throw new SimulationException(e);
+		}
+	}
+}
