9da62e64ea6007dae7b8d2d3ca107916737d5891
ljmay <ljmay@10.0.0.65>
2021-04-06 16:20:25 -0600

    creation of use cases

---
+++ SelfCheckoutSystem - Software/src/ApproveWeightDiscrepency.java
@@ -0,0 +1,8 @@
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+
+public class ApproveWeightDiscrepency {
+
+	public ApproveWeightDiscrepency(SelfCheckoutStation station) {
+		
+	}
+}
---
+++ SelfCheckoutSystem - Software/src/EnterPLU.java
@@ -0,0 +1,8 @@
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+
+public class EnterPLU {
+
+	public EnterPLU(SelfCheckoutStation station) {
+		
+	}
+}
