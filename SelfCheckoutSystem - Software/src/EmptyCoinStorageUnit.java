2850f077d812a85562373c96f6d418ac9602510b
maboz <mabozheng@gmail.com>
2021-04-07 00:22:15 -0600

    test

--- SelfCheckoutSystem - Software/src/EmptyCoinStorageUnit.java
+++ SelfCheckoutSystem - Software/src/EmptyCoinStorageUnit.java
@@ -1,4 +1,19 @@
+
+import org.lsmr.selfcheckout.devices.*;
+import org.lsmr.selfcheckout.Coin;
 public class EmptyCoinStorageUnit {
+
+    public EmptyCoinStorageUnit()
+    {
+
+    }
+
+    void AttendantEmptyCoinStorageUnit()
+    {
+        CoinStorageUnit CSU = new CoinStorageUnit();
+        CSU.unload();
+
+    }
 
 
 
