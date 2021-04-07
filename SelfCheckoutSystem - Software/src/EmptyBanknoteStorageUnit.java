a4973f1ae68e802c312b19bb79722bf3f9256c61
maboz <mabozheng@gmail.com>
2021-04-06 23:52:23 -0600

    Added files

---
+++ SelfCheckoutSystem - Software/src/EmptyBanknoteStorageUnit.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Currency;
+import org.lsmr.selfcheckout.Banknote;
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.BanknoteValidator;
+import org.lsmr.selfcheckout.devices.DisabledException;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SimulationException;
+import org.lsmr.selfcheckout.devices.listeners.AbstractDeviceListener;
+import org.lsmr.selfcheckout.devices.listeners.BanknoteValidatorListener;
+
+public class EmptyBanknoteStorageUnit{
+
+
+}
