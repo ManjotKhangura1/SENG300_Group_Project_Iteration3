db734b83d2aa2283b3f84116045804d8a0311698
hqiao8 <qiaohaipeng014@gmail.com>
2021-04-08 17:52:59 -0600

    Enter Membership Test

---
+++ SelfCheckoutSystem - Tests/src/EnterMembershipTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class EnterMembershipTest {
+	@Test
+	public void testFirstCard() {
+		EnterMembership test = new EnterMembership("1234567");
+		assertNotNull(test.getCurrentCard());
+	}
+	
+	@Test
+	public void testSecondCard() {
+		EnterMembership test = new EnterMembership("2345678");
+		assertNotNull(test.getCurrentCard());
+	}
+	
+	@Test
+	public void testInvalidCard() {
+		EnterMembership test = new EnterMembership("3456789");
+		assertNull(test.getCurrentCard());
+	}
+}
