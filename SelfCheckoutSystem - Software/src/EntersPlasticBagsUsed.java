309a2588d33af802c20bf61615b44ddcb99ae21a
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2021-04-08 23:57:17 -0600

    Added methods: Prompt + Charge for plastic bags

--- SelfCheckoutSystem - Software/src/EntersPlasticBagsUsed.java
+++ SelfCheckoutSystem - Software/src/EntersPlasticBagsUsed.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
 public class EntersPlasticBagsUsed {
-
-	double pricePerPlasticBag = 0.10; //10 cents charge per plastic bag 
-	double totalPlasticBagsUsed;
-	double totalPlasticBagsUsedPrice;
+		
+	
+	public double pricePerPlasticBag = 0.10; //10 cents charge per plastic bag 
+	public int totalPlasticBagsUsed;
+	public double totalPlasticBagsUsedPrice;
 	
 	public int askNumOfPlasticBagsUsed() {
+		
 		System.out.println("Enter the total number of plastic bags used: ");
 		Scanner scan= new Scanner(System.in);
-		
-		
-		return 0;
+		totalPlasticBagsUsed = scan.nextInt();
+		return totalPlasticBagsUsed;
 	}
 	
+	public double calculateTotalPlasticBagsUsedPrice() {
+	
+		totalPlasticBagsUsedPrice = 0.10 * this.totalPlasticBagsUsed;
+		return totalPlasticBagsUsedPrice;
+	
+	}
 }
