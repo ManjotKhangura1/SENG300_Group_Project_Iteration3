b153a07dc88ac31c3f9f710b061eb935116c51fb
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2021-04-08 23:54:44 -0600

    # Plastic Bags Used

---
+++ SelfCheckoutSystem - Software/src/EntersPlasticBagsUsed.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EntersPlasticBagsUsed {
+
+	double pricePerPlasticBag = 0.10; //10 cents charge per plastic bag 
+	double totalPlasticBagsUsed;
+	double totalPlasticBagsUsedPrice;
+	
+	public int askNumOfPlasticBagsUsed() {
+		System.out.println("Enter the total number of plastic bags used: ");
+		Scanner scan= new Scanner(System.in);
+		
+		
+		return 0;
+	}
+	
+}
