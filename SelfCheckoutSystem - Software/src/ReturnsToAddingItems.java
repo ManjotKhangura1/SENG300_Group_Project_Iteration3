d13e45d64907c5a2fac9f552252dfbcb4253a2dc
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2021-04-10 10:42:16 -0600

    BarcodeScanner stub created

--- SelfCheckoutSystem - Software/src/ReturnsToAddingItems.java
+++ SelfCheckoutSystem - Software/src/ReturnsToAddingItems.java
@@ -5,10 +5,25 @@ import org.lsmr.selfcheckout.*;
 
 public class ReturnsToAddingItems {
 	
+	public SelfCheckoutStation aSelfCheckoutStation;
+	public boolean isScanned;
+	public boolean isEnabled;
 	
-	
-	    
-	    
+	 private BarcodeScannerListener bsl = new BarcodeScannerListener() {
+		 
+	        @Override
+	        public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+	        }
 
-	
+	        @Override
+	        public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+	        }
+		 
+	        @Override
+	        public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
+	            // TODO Auto-generated method stub
+	        	isScanned = true;  	    
+	        }
+	    };
+	   	    	
 }
