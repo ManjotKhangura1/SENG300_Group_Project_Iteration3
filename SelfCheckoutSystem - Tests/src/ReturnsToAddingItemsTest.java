4a0349c75b80c2ec91e9b662b5d758fdb5fd6c33
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2021-04-10 11:40:55 -0600

    Test skeleton

---
+++ SelfCheckoutSystem - Tests/src/ReturnsToAddingItemsTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Currency;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lsmr.selfcheckout.Card;
+import org.lsmr.selfcheckout.Coin;
+import org.lsmr.selfcheckout.Item;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SimulationException;
+
+public class ReturnsToAddingItemsTest {
+	
+	//parameters for instance of valid SelfCheckoutStation
+	Currency currency= Currency.getInstance("CAD");
+	int[] bankNoteDenominations= {5,10,20,50,100};
+	BigDecimal[] coinDenominations= {new BigDecimal(0.01), new BigDecimal(0.05),
+			 new BigDecimal(0.1),new BigDecimal(0.25), new BigDecimal(0.50)};
+	int scaleMaximumWeight= 1000;
+	int scaleSensitivity=1;
+	
+	SelfCheckoutStation validStation = new SelfCheckoutStation(currency, bankNoteDenominations,
+			coinDenominations, scaleMaximumWeight, scaleSensitivity);
+	
+	ReturnsToAddingItems return1 = new ReturnsToAddingItems(validStation);
+	
+	
+	
+}
