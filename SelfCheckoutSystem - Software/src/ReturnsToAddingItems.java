50a2f55b1bf8477f705e5b02de4d99d914b25afa
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2021-04-10 11:31:05 -0600

    Added method that enables

--- SelfCheckoutSystem - Software/src/ReturnsToAddingItems.java
+++ SelfCheckoutSystem - Software/src/ReturnsToAddingItems.java
@@ -5,15 +5,22 @@ import org.lsmr.selfcheckout.*;
 
 public class ReturnsToAddingItems {
 	
-	//variables for the class
+	/*
+	 * variables for the class
+	 */
 	public SelfCheckoutStation aSelfCheckoutStation;
 	
-	//initially false
+	/*
+	 * initially false
+	 */
 	public boolean isScanned=false;
 	public boolean isEnabled=false;
 
-	//Creating instance of BarcodeScannerListener as it is required if customer wants to 
-	//continue adding items
+	/*
+	 * Creating instance of BarcodeScannerListener as it is required if customer wants to 
+	 * continue adding items
+	 */
+	
 	private BarcodeScannerListener bsl = new BarcodeScannerListener() {
 		 
 	
@@ -30,13 +37,18 @@ public class ReturnsToAddingItems {
         @Override
         public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
             // TODO Auto-generated method stub
+        	//this will make sure (once they've returned to adding items), if items
+        	//successfully get scanned or not
         	isScanned = true;  	    
         }
     };
    
-	//Constructor for the class
-    //Must pass a SelfCheckoutStation as the program makes use of methods and variables
-    //in SelfCheckoutStation
+	/*
+	 * Constructor for the class
+	 * Must pass a SelfCheckoutStation as the program makes use of methods and variables
+	 * in SelfCheckoutStation
+	 */
+    
     public void ReturnsToAddingItems(SelfCheckoutStation aStation) {
     	
     	//Station passed cannot be null
@@ -51,15 +63,19 @@ public class ReturnsToAddingItems {
     	aSelfCheckoutStation.mainScanner.register(bsl);
     	aSelfCheckoutStation.handheldScanner.register(bsl);
     	
-    	//Enabling every aspect in the self checkout system that is required for the
-    	//process of customer adding items to be successful
-    	//Enabling these so the customer can make use of them to successfully add an item
+    }
+	    
+    /*
+     * Enabling every aspect in the self checkout system that is required for the
+     * process of customer adding items to be successful
+     * Enabling these so the customer can make use of them to successfully add an item
+     */
+	
+    public void enableRequiredHardware() {
+    	
     	aSelfCheckoutStation.mainScanner.enable();
     	aSelfCheckoutStation.handheldScanner.enable();
     	aSelfCheckoutStation.baggingArea.enable();
     	aSelfCheckoutStation.scale.enable();
-    	
-    	
-    }
-	   	
+    }	
 }
