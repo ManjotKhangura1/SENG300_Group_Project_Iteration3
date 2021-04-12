5871257e3b7637291bbd741daf2c55ee46d428e3
Danieljin2001 <Djin2001@gmail.com>
2021-04-12 14:29:38 -0600

    - Changed EntersPlasticBagsUsed from user input to setters and getters so that i can be asked in the main instead
    - Added EntersPlasticBagsUsedTest

---
+++ SelfCheckoutSystem - Tests/src/EntersPlasticBagsUsedTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EntersPlasticBagsUsedTest {
+@Test
+public void setTotalPlasticBagsUsed_WithPostiveNumberTest() {
+	EntersPlasticBagsUsed epbu = new EntersPlasticBagsUsed();
+	int test = 1;
+	int expected = 1;
+	epbu.setTotalPlasticBagsUsed(test);
+	int actual = epbu.getTotalPlasticBagsUsed();
+	assertEquals(expected, actual);
+}
+
+@Test
+public void setTotalPlasticBagsUsed_WithNegativeNumberTest() {
+	EntersPlasticBagsUsed epbu = new EntersPlasticBagsUsed();
+	int test = -1;
+	int expected = 0;
+	epbu.setTotalPlasticBagsUsed(test);
+	int actual = epbu.getTotalPlasticBagsUsed();
+	assertEquals(expected, actual);
+}
+
+
+@Test
+public void getTotalPlasticBagsUsed() {
+	EntersPlasticBagsUsed epbu = new EntersPlasticBagsUsed();
+	int test = 0;
+	int expected = 0;
+	epbu.setTotalPlasticBagsUsed(test);
+	int actual = epbu.getTotalPlasticBagsUsed();
+	assertEquals(expected, actual);
+}
+
+@Test
+public void calculateTotalPlasticBagsUsedPrice() {
+	EntersPlasticBagsUsed epbu = new EntersPlasticBagsUsed();
+	int testBagAmount = 3;
+	double pricePerBag = 0.10;
+	double expected = pricePerBag * testBagAmount;
+	epbu.setTotalPlasticBagsUsed(testBagAmount);
+	double actual = epbu.calculateTotalPlasticBagsUsedPrice();
+	assertEquals(expected, actual, 0);
+}
+}
