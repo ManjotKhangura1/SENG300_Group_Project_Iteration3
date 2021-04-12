83fbfa2b7c60468d3f0aeffd0ec6951a6ca39bb6
monica-nguyen <monica.nguyen1@ucalgary.ca>
2021-04-12 06:14:35 -0600

    Fixed 'Defaults' in JLabels and Added BaggingAreaPanel

--- SelfCheckoutSystem - Software/src/stationGUI/AddItemPanel.java
+++ SelfCheckoutSystem - Software/src/stationGUI/AddItemPanel.java
@@ -28,9 +28,7 @@ import javax.swing.JTextField;
 public class AddItemPanel extends JPanel{
 	
 	private MainFrame mainFrame;
-
 	private JTextField txtScanned;
-
 
 	
 	public AddItemPanel(MainFrame mainFrame)
@@ -53,7 +51,12 @@ public class AddItemPanel extends JPanel{
 		
 		TextField textField_2 = new TextField(22);
 		add(textField_2, "cell 2 14");
+		
 
+
+
+	
+		
 	}
 
 	private void initButtons() {
@@ -135,78 +138,81 @@ public class AddItemPanel extends JPanel{
 		});
 	
 		
+
+		
 	}
 
 	private void initLabels() {
 		
-		JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("Add Bags");
+		
+		JLabel lblNewJgoodiesLabel = new JLabel("Add Bags");
 		lblNewJgoodiesLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		add(lblNewJgoodiesLabel, "cell 1 1");
 		
-		JLabel lblNewJgoodiesLabel_15 = DefaultComponentFactory.getInstance().createLabel("PLU Code Guide");
+		JLabel lblNewJgoodiesLabel_15 = new JLabel("PLU Code Guide");
 		lblNewJgoodiesLabel_15.setFont(new Font("Tahoma", Font.BOLD, 20));
 		add(lblNewJgoodiesLabel_15, "cell 5 1");
 		
-		JLabel lblNewJgoodiesLabel_14 = DefaultComponentFactory.getInstance().createLabel("Enter number of bags: ");
+		JLabel lblNewJgoodiesLabel_14 = new JLabel("Enter number of bags: ");
 		lblNewJgoodiesLabel_14.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_14, "cell 1 2");
 		TextField textField = new TextField(22);
 		add(textField, "cell 1 3");
 		
-		JLabel lblNewJgoodiesLabel_2 = DefaultComponentFactory.getInstance().createLabel("Item: ");
+		JLabel lblNewJgoodiesLabel_2 = new JLabel("Item: ");
 		lblNewJgoodiesLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_2, "cell 5 3");
 		
-		JLabel lblNewJgoodiesLabel_13 = DefaultComponentFactory.getInstance().createLabel("Code:");
+		JLabel lblNewJgoodiesLabel_13 = new JLabel("Code:");
 		lblNewJgoodiesLabel_13.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_13, "cell 6 3");
 		
-		JLabel lblNewJgoodiesLabel_3 = DefaultComponentFactory.getInstance().createLabel("Scan Item:");
+		JLabel lblNewJgoodiesLabel_3 = new JLabel("Scan Item:");
 		lblNewJgoodiesLabel_3.setFont(new Font("Tahoma", Font.BOLD, 20));
 		add(lblNewJgoodiesLabel_3, "cell 1 5");
 		
-		JLabel lblNewJgoodiesLabel_4 = DefaultComponentFactory.getInstance().createLabel("Onions");
+		JLabel lblNewJgoodiesLabel_4 = new JLabel("Onions");
 		lblNewJgoodiesLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_4, "cell 5 5");
 		
-		JLabel lblNewJgoodiesLabel_8 = DefaultComponentFactory.getInstance().createLabel("14040");
+		JLabel lblNewJgoodiesLabel_8 = new JLabel("14040");
 		lblNewJgoodiesLabel_8.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_8, "cell 6 5");
 		
-		JLabel lblNewJgoodiesLabel_5 = DefaultComponentFactory.getInstance().createLabel("Potatoes");
+		JLabel lblNewJgoodiesLabel_5 = new JLabel("Potatoes");
 		lblNewJgoodiesLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_5, "cell 5 7");
 		
-		JLabel lblNewJgoodiesLabel_9 = DefaultComponentFactory.getInstance().createLabel("97310");
+		JLabel lblNewJgoodiesLabel_9 = new JLabel("97310");
 		lblNewJgoodiesLabel_9.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewJgoodiesLabel_9.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_9, "cell 6 7");
 		
-		JLabel lblNewJgoodiesLabel_6 = DefaultComponentFactory.getInstance().createLabel("Apples");
+		JLabel lblNewJgoodiesLabel_6 = new JLabel("Apples");
 		lblNewJgoodiesLabel_6.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_6, "cell 5 9");
 		
-		JLabel lblNewJgoodiesLabel_10 = DefaultComponentFactory.getInstance().createLabel("55589");
+		JLabel lblNewJgoodiesLabel_10 = new JLabel("55589");
 		lblNewJgoodiesLabel_10.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_10, "cell 6 9");
 		
-		JLabel lblNewJgoodiesLabel_7 = DefaultComponentFactory.getInstance().createLabel("Oranges");
+		JLabel lblNewJgoodiesLabel_7 = new JLabel("Oranges");
 		lblNewJgoodiesLabel_7.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_7, "cell 5 11");
 		
-		JLabel lblNewJgoodiesLabel_11 = DefaultComponentFactory.getInstance().createLabel("30897");
+		JLabel lblNewJgoodiesLabel_11 = new JLabel("30897");
 		lblNewJgoodiesLabel_11.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_11, "cell 6 11");
 		
-		JLabel lblNewJgoodiesLabel_12 = DefaultComponentFactory.getInstance().createLabel("Add PLU Item:");
+		JLabel lblNewJgoodiesLabel_12 = new JLabel("Add PLU Item:");
 		lblNewJgoodiesLabel_12.setFont(new Font("Tahoma", Font.BOLD, 20));
 		add(lblNewJgoodiesLabel_12, "cell 1 12");
 				
-		JLabel lblNewJgoodiesLabel_1 = DefaultComponentFactory.getInstance().createLabel("Enter PLU Code:");
+		JLabel lblNewJgoodiesLabel_1 = new JLabel("Enter PLU Code:");
 		lblNewJgoodiesLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_1, "cell 1 13");
 		
-		JLabel lblNewJgoodiesLabel_16 = DefaultComponentFactory.getInstance().createLabel("Enter Amount(g):");
+		JLabel lblNewJgoodiesLabel_16 = new JLabel("Enter Amount(g):");
 		lblNewJgoodiesLabel_16.setFont(new Font("Tahoma", Font.BOLD, 12));
 		add(lblNewJgoodiesLabel_16, "cell 2 13");
 				
@@ -217,8 +223,8 @@ public class AddItemPanel extends JPanel{
 		setBounds(0,0,1280,720);
 		setLayout(new MigLayout("", "[][][][][grow][][][grow]", "[][][][][][][][][][][][][][][][][][][grow][][][][][][][][][][][][]"));
 		setVisible(false);
-	
-	}
+		
 
+	}
 
 }
--- SelfCheckoutSystem - Software/src/stationGUI/BaggingAreaPanel.java
+++ SelfCheckoutSystem - Software/src/stationGUI/BaggingAreaPanel.java
@@ -1,25 +1,111 @@
 package stationGUI;
 
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class BaggingAreaPanel extends JPanel {
 	
+	
 	private MainFrame mainFrame;
 	
-	public BaggingAreaPanel(MainFrame mainFrame)
-	{
+	public BaggingAreaPanel(MainFrame mainFrame) {
+		
 		this.mainFrame = mainFrame;
 		
 		initComponents();
+		initLabels();
+		initButtons();
+		
 	}
+	
+
+
+	private void initButtons() {
+		
+		JButton btnNewButton = new JButton("Back to Adding Items");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		add(btnNewButton, "cell 1 3");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.addItemPanel.setVisible(true);
+				mainFrame.baggingAreaPanel.setVisible(false);
+				mainFrame.attendantLoginPanel.setVisible(false);
+			}
+		});
+		
+		
+		JButton btnNewButton_2 = new JButton("View Cart");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.baggingAreaPanel.setVisible(false);
+				mainFrame.scanningPanel.setVisible(true);
+			}
+		});
+		
+		btnNewButton_2.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		add(btnNewButton_2, "cell 1 4");
+		
+	}
+
+
 
 	private void initComponents()
 	{
 		setBounds(0,0,1280,720);
-		setLayout(new MigLayout("","[]","[]"));
+		setLayout(new MigLayout("", "[517.00][94.00][200.00,grow][17.00,grow][180.00][204.00,grow][17.00][][][][][][][][][][][][][][][][38.00][36.00,grow]", "[139.00,grow][129.00,grow][138.00,grow][134.00,grow][135.00,grow][124.00,grow]"));
 		setVisible(false);
-	}
+		
 
+		
+	}
+	
+	private void initLabels() {
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, "cell 0 0 1 6,grow");
+		
+		JPanel panel_1 = new JPanel();
+		scrollPane.setViewportView(panel_1);
+		
+		JLabel lblNewLabel = new JLabel("Bagging Area: ");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		scrollPane.setColumnHeaderView(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Weight of Item Added: ");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		add(lblNewLabel_1, "cell 1 0");
+		
+		JPanel panel_2 = new JPanel();
+		add(panel_2, "cell 2 0,grow");
+		
+		JLabel lblNewLabel_2 = new JLabel("Total Weight:");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		add(lblNewLabel_2, "cell 1 1");
+		
+		JPanel panel_3 = new JPanel();
+		add(panel_3, "cell 2 1,grow");
+		
+		JLabel lblNewLabel_3 = new JLabel("Station Status:");
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		add(lblNewLabel_3, "cell 1 2");
+		
+		JPanel panel_4 = new JPanel();
+		add(panel_4, "cell 2 2,grow");
+		
+		
+		
+	}
+	
 }
